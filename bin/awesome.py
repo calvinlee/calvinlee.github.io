ea572cb7e18d3e7c0922c92c96dbf3951fe70ac5
hao_li <calvinlee@cienet.com.cn>
2013-11-07 09:56:39 +0800

    Site updated at 2013-11-07 01:56:39 UTC

--- bin/awesome.py
+++ bin/awesome.py
@@ -5,7 +5,9 @@ import os, sys
 for root, dirs, files in os.walk("_posts"):
     for filespath in files:
         file_path = os.path.join(root, filespath)
-        file_h = open(file_path)
-        line = file_h.readline()
-        while line
+        #os.system(r"sed -i '/include JB/d' %s" % file_path)
+        fileName, fileExtension = os.path.splitext(file_path)
+        newName = fileName + ".markdown"
+        os.rename(file_path, newName)
+        print newName
 
